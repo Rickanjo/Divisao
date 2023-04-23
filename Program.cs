d495704620c79cfaba78ce01af9848db12d94e8e
Rickanjo <Ricardo.cassettari07@gmail.com>
2023-04-23 11:59:00 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// Leia um número inteiro e exiba "Negativo", "Zero" ou "Positivo", conforme o caso.
+
+Console.WriteLine("--- Continha de divisão ---\n");
+
+Console.Write("Digite o numerador.......: ");
+int numerador = int.Parse(Console.ReadLine()!);
+
+Console.Write("Digite o denominador.....: ");
+int denominador = int.Parse(Console.ReadLine()!);
+
+if (denominador == 0) {
+    Console.WriteLine("Não é possível dividir por zero.");
+} else {
+     double resultado = (double)numerador / denominador;
+    Console.WriteLine($"Resultado da divisão: {resultado}");
+}
+
+
